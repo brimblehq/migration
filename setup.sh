1e34f66ba90034e04e6b33a4f669fcb866fed472
Muritala David <davmuri1414@gmail.com>
2024-11-01 02:07:31 +0100

    nvm in script fix

--- setup.sh
+++ setup.sh
@@ -171,12 +171,30 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 sudo apt install docker-compose -y
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+NVM_INIT='export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
+
+if ! grep -q "NVM_DIR" ~/.bashrc; then
+    echo "$NVM_INIT" >> ~/.bashrc
+    echo "Added NVM initialization to .bashrc"
+fi
+
 source ~/.bashrc
+
+command -v nvm
+
 nvm install --lts
+nvm use --lts
 
 npm install --global yarn
-
 npm install -g pm2
+
 
 curl -1sLf \
 'https://dl.cloudsmith.io/public/infisical/infisical-cli/setup.deb.sh' \
