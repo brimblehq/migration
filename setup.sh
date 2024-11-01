73fac91ec330f7cfd6f3d941dc70865862f66b05
Muritala David <davmuri1414@gmail.com>
2024-11-01 01:57:24 +0100

    minor

--- setup.sh
+++ setup.sh
@@ -284,15 +284,13 @@ sudo ufw enable
 source ~/.bashrc
 
 sudo apt-get update && \
-  sudo apt-get install wget gpg coreutils
-
+  sudo apt-get install wget gpg coreutils -y
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
   sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
 | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
 
 sudo apt-get update -y && sudo apt-get install nomad
 
