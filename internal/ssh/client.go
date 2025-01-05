7d19109e5aa588dcbb95edd3bdb042436464e983
Muritala David <davmuri1414@gmail.com>
2025-01-05 23:04:07 +0100

    improved ssh host authentication

--- internal/ssh/client.go
+++ internal/ssh/client.go
@@ -73,48 +73,6 @@ func createConfigFromKeyPath(server types.Server) (*ssh.ClientConfig, error) {
 	}, nil
 }
 
-// func NewSSHClient(server types.Server) (*SSHClient, error) {
-// 	keyPath := server.KeyPath
-
-// 	if strings.HasPrefix(keyPath, "~") {
-// 		homeDir, err := os.UserHomeDir()
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to resolve home directory: %v", err)
-// 		}
-// 		keyPath = filepath.Join(homeDir, keyPath[1:])
-// 	}
-
-// 	key, err := ioutil.ReadFile(keyPath)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("%v: unable to read private key: %v", server, err)
-// 	}
-
-// 	passphrase := []byte("password")
-// 	signer, err := ssh.ParsePrivateKeyWithPassphrase(key, passphrase)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("unable to parse private key: %v", err)
-// 	}
-
-// 	config := &ssh.ClientConfig{
-// 		User: server.Username,
-// 		Auth: []ssh.AuthMethod{
-// 			ssh.PublicKeys(signer),
-// 		},
-// 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-// 		Timeout:         10 * time.Second,
-// 	}
-
-// 	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:22", server.Host), config)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to connect to %s: %v", server.Host, err)
-// 	}
-
-// 	return &SSHClient{
-// 		Client: client,
-// 		config: config,
-// 	}, nil
-// }
-
 func (s *SSHClient) ExecuteCommand(command string) error {
 	session, err := s.Client.NewSession()
 	if err != nil {
