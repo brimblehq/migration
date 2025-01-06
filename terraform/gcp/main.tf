56d5f2d33a793719129a796da644d1f25c8ab80e
Fife <fife.team005@gridirontest.com>
2025-01-07 00:02:11 +0100

    Writing config scripts for GCP

---
+++ terraform/gcp/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.15.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# VPC Network
+resource "google_compute_network" "nomad_network" {
+  name                    = "nomad-network"
+  auto_create_subnetworks = false
+}
+
+# Subnet
+resource "google_compute_subnetwork" "nomad_subnet" {
+  name          = "nomad-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.nomad_network.id
+  region        = var.region
+}
+
+# Firewall Rules
+resource "google_compute_firewall" "nomad_firewall" {
+  name    = "nomad-firewall"
+  network = google_compute_network.nomad_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "4646", "4647", "4648", "8500", "8600"] # SSH, Nomad, Consul
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["nomad-instance"]
+}
+
+# Instance Template
+resource "google_compute_instance_template" "nomad_template" {
+  name        = "nomad-instance-template"
+  machine_type = var.machine_type
+
+  disk {
+    source_image = var.image
+    boot         = true
+    auto_delete  = true
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.nomad_subnet.id
+    access_config {} # Adds external IP
+  }
+
+  metadata = {
+    ssh-keys = "nomad-user:${file(var.public_ssh_key_path)}"
+  }
+
+  tags = ["nomad-instance"]
+}
+
+# Managed Instance Group
+resource "google_compute_instance_group_manager" "nomad_instances" {
+  name               = "nomad-instance-group"
+  base_instance_name = "nomad-instance"
+  instance_template  = google_compute_instance_template.nomad_template.id
+  target_size        = var.instance_count
+
+  named_port {
+    name = "nomad"
+    port = 4646
+  }
+}
+
+# Output Instance Group Information
+output "instance_group_name" {
+  value = google_compute_instance_group_manager.nomad_instances.name
+}
+
+output "instance_template_name" {
+  value = google_compute_instance_template.nomad_template.name
+}
---
+++ terraform/gcp/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+  default = "Nomad-setup"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "Machine type for Nomad instances"
+  type        = string
+  default     = "e2-standard-2"
+}
+
+variable "image" {
+  description = "Source image for the instance template"
+  type        = string
+  default     = "projects/debian-cloud/global/images/family/debian-11"
+}
+
+variable "instance_count" {
+  description = "Number of Nomad instances"
+  type        = number
+  default     = 3
+}
+
+variable "public_ssh_key_path" {
+  description = "Path to the public SSH key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
